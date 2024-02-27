6ff00d0b1b0ab646f8d2bf591571517c5db45818
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 05:34:00 +0300

    Add Firebase authentication and log the user to the console.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import Navbar from "./components/Navbar";
 
+import { auth } from "./firebase/config";
+import { useAuthState } from "react-firebase-hooks/auth";
+
 const App = () => {
+  const [user] = useAuthState(auth);
+  console.log(user);
+
   return (
     <div className="max-w-[728px] mx-auto text-center">
       <section className="flex flex-col h-[90vh] bg-backgroundColor mt-10 shadow-xl border relative">
