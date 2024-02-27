a8bc0439f279a3c1e5b3271d361c04cff9cf5ad4
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 13:29:18 +0300

    -

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import SignIn from "./SignIn";
+import LogOut from "./LogOut";
 import { auth } from "../firebase/config";
 import { useAuthState } from "react-firebase-hooks/auth";
-import LogOut from "./LogOut";
 
 const Navbar = () => {
   const [user] = useAuthState(auth);
+
   return (
     <div className="bg-chatInputColor h-20 flex justify-between items-center p-4">
       <h1 className="text-darkGrayColor text-3xl">Chat App</h1>
