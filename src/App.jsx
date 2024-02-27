5eda02893e3e8499fca65ded41cbe2b43364b9ad
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 04:58:10 +0300

    Add Navbar component and basic layout for the chat app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <div className="max-w-[728px] mx-auto text-center">
+      <section className="flex flex-col h-[90vh] bg-backgroundColor mt-10 shadow-xl border relative">
+        {/* Navbar */}
+        <Navbar />
+        {/* Chat component */}
+      </section>
+    </div>
+  );
 };
 
 export default App;
