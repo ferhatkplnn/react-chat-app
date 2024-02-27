5d06f3742d65089d4b336a84ceb3f35eeb951e74
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 13:36:37 +0300

    Update CSS class names for Message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,23 +1,22 @@
 import { auth } from "../firebase/config";
 
-const style = {
-  message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
-  name: `absolute mt-[-4rem] text-gray-600 text-xs`,
-  sent: `bg-sentMessageColor text-white flex-row-reverse text-end float-right rounded-br-full`,
-  received: `bg-receivedMessageColor text-gray-900 float-left rounded-br-full`,
+const styles = {
+  message:
+    "flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full",
+  name: "absolute mt-[-4rem] text-gray-600 text-xs",
+  sent: "bg-sentMessageColor text-white flex-row-reverse text-end float-right rounded-br-full",
+  received: "bg-receivedMessageColor text-gray-900 float-left rounded-br-full",
 };
 
 const Message = ({ message }) => {
-  const messageClass =
-    message.uid === auth.currentUser.uid
-      ? `${style.sent}`
-      : `${style.received}`;
+  const isCurrentUser = message.uid === auth.currentUser.uid;
+  const messageClass = isCurrentUser ? styles.sent : styles.received;
 
   return (
     <div>
-      <div className={`${style.message} ${messageClass}`}>
-        <p className={style.name}>{message.name}</p>
-        <p className="">{message.text}</p>
+      <div className={`${styles.message} ${messageClass}`}>
+        <p className={styles.name}>{message.name}</p>
+        <p>{message.text}</p>
       </div>
     </div>
   );
