6dc1365c5e87ac5afb4df41e610ce35ab967d914
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 04:58:00 +0300

    Create Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="bg-chatInputColor h-20 flex justify-between items-center p-4">
+      <h1 className="text-darkGrayColor text-3xl">Chat App</h1>
+    </div>
+  );
+};
+
+export default Navbar;
