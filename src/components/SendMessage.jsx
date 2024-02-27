ce26c5cea6c8e9f087ee695e2e317e16d3b812f5
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 13:17:14 +0300

    Update SendMessage component to use sendMessageToFirebase function

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,35 +1,37 @@
 import { useState } from "react";
-import { auth, db } from "../firebase/config";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { sendMessageToFirebase } from "../firebase/actions";
 
 const SendMessage = ({ scroll }) => {
   const [input, setInput] = useState("");
 
   const sendMessage = async (e) => {
     e.preventDefault();
-    if (!auth.currentUser || !input) return;
-    const { uid, displayName } = auth.currentUser;
-    await addDoc(collection(db, "messages"), {
-      text: input,
-      name: displayName,
-      uid,
-      timestamp: serverTimestamp(),
-    });
+
+    if (!input) return;
+
+    const isSending = await sendMessageToFirebase(input);
+
+    if (!isSending) return;
 
     setInput("");
     scroll.current.scrollIntoView({ behavior: "smooth" });
   };
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
   return (
     <form
       onSubmit={sendMessage}
-      className="h-14 w-full max-w-[728px] flex text-xl absolute bottom-0"
+      className="flex h-14 w-full max-w-[728px] absolute bottom-0 text-xl"
     >
       <input
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
         type="text"
+        value={input}
+        onChange={handleInputChange}
         placeholder="Message"
-        className="w-full text-xl p-3 bg-chatInputColor text-darkGrayColor  outline-none border-none"
+        className="w-full p-3 text-xl bg-chatInputColor text-darkGrayColor outline-none border-none"
       />
       <button type="submit" className="w-[20%] bg-red-600 text-white">
         Send
