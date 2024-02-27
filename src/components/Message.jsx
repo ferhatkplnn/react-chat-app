14c435c129dc786c07c2d6229c2ef3bbd7846543
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 06:01:05 +0300

    Add message template

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Message = () => {
-  return <div>Message</div>;
+const Message = ({ message }) => {
+  return (
+    <div>
+      <div className="flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full">
+        <p className="fixed mt-[-4rem] text-gray-600 text-xs">Dave</p>
+        <p>{message.text}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
