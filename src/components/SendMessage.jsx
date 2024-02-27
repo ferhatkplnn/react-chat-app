7a45494e3894631e604e42f49d7d11a79094c72f
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 06:07:00 +0300

    Add a message sending form to the bottom of the screen.

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const SendMessage = () => {
-  return <div>SendMessage</div>;
+  return (
+    <form className="h-14 w-full max-w-[728px] flex text-xl absolute bottom-0">
+      <input
+        type="text"
+        placeholder="Message"
+        className="w-full text-xl p-3 bg-gray-900 text-white outline-none border-none"
+      />
+      <button type="submit" className="w-[20%] bg-green-500">
+        Send
+      </button>
+    </form>
+  );
 };
 
 export default SendMessage;
