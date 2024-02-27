b4ea30a7fa2d9da96cc278746fd9065285e29abb
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 06:01:20 +0300

    Add Firebase integration to display real-time messages.

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
+import Message from "./Message";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase/config";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const [messages, setMessages] = useState([]);
+  const scroll = useRef(null);
+
+  useEffect(() => {
+    const q = query(collection(db, "messages"), orderBy("timestamp"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let messagesTemp = [];
+      querySnapshot.forEach((doc) => {
+        messagesTemp.push({
+          ...doc.data(),
+          id: doc.id,
+        });
+      });
+      setMessages(messagesTemp);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <>
+      <main className="flex flex-col p-[10px] relative">
+        {messages &&
+          messages.map((message) => (
+            <Message key={message.id} message={message} />
+          ))}
+      </main>
+      {/* Send Message Component */}
+      <span ref={scroll}></span>
+    </>
+  );
 };
 
 export default Chat;
