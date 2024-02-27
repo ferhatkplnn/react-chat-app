1ca77cea5f7b164dcfb7d9eef8e336c147d50ab5
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 14:26:53 +0300

    Remove unnecessary console.log statement

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { useAuthState } from "react-firebase-hooks/auth";
 
 const App = () => {
   const [user] = useAuthState(auth);
-  console.log(user);
 
   return (
     <div className="max-w-[728px] mx-auto text-center">
