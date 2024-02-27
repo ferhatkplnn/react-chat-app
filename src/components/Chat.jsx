6948317efdf8c0d2b26d74f4829d8e0f04d874a0
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 13:52:03 +0300

    Update messages query and add loading state.

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,39 +1,44 @@
 import { useEffect, useRef, useState } from "react";
-import Message from "./Message";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { db } from "../firebase/config";
+import Message from "./Message";
 import SendMessage from "./SendMessage";
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(true);
   const scroll = useRef(null);
 
   useEffect(() => {
-    const q = query(collection(db, "messages"), orderBy("timestamp"));
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      let messagesTemp = [];
-      querySnapshot.forEach((doc) => {
-        messagesTemp.push({
-          ...doc.data(),
-          id: doc.id,
-        });
-      });
-      setMessages(messagesTemp);
+    const messagesQuery = query(
+      collection(db, "messages"),
+      orderBy("timestamp")
+    );
+
+    const unsubscribe = onSnapshot(messagesQuery, (querySnapshot) => {
+      const updatedMessages = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+
+      setMessages(updatedMessages);
+      setLoading(false);
     });
 
     return () => unsubscribe();
   }, []);
 
+  const renderMessages = () =>
+    messages.map((message) => <Message key={message.id} message={message} />);
+
   return (
     <>
       <main className="flex flex-col p-[10px] pb-14 relative overflow-auto">
-        {messages &&
-          messages.map((message) => (
-            <Message key={message.id} message={message} />
-          ))}
+        {renderMessages()}
         <span ref={scroll}></span>
       </main>
-      {/* Send Message Component */}
+
+      {loading && <div className="text-center">Loading...</div>}
       <SendMessage scroll={scroll} />
     </>
   );
