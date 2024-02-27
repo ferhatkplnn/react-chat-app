ef2394a3802dfe58e1e79c90ecce71e8fed51ad3
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 13:24:48 +0300

    Replace Google sign-in with `googleSignInToFirebase` function.

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,21 +1,11 @@
 // Import necessary Firebase and authentication components
-import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 import GoogleButton from "react-google-button";
-import { auth } from "../firebase/config";
-
-const googleSignIn = async () => {
-  const provider = new GoogleAuthProvider();
-  try {
-    await signInWithRedirect(auth, provider);
-  } catch (error) {
-    console.error("Error signing in with Google:", error);
-  }
-};
+import { googleSignInToFirebase } from "../firebase/actions";
 
 const SignIn = () => {
   return (
     <div className="flex justify-center">
-      <GoogleButton onClick={googleSignIn} />
+      <GoogleButton onClick={googleSignInToFirebase} />
     </div>
   );
 };
