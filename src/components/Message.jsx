d9e0e7003c98a0d732565f1e72ebc92a9514f43d
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-27 11:50:20 +0300

    Add styling to Message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { auth } from "../firebase/config";
+
+const style = {
+  message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
+  name: `absolute mt-[-4rem] text-gray-600 text-xs`,
+  sent: `bg-sentMessageColor text-darkGrayColor flex-row-reverse text-end float-right rounded-br-full`,
+  received: `bg-receivedMessageColor text-black float-left rounded-br-full`,
+};
 
 const Message = ({ message }) => {
+  const messageClass =
+    message.uid === auth.currentUser.uid
+      ? `${style.sent}`
+      : `${style.received}`;
+
   return (
     <div>
-      <div className="flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full">
-        <p className="fixed mt-[-4rem] text-gray-600 text-xs">Dave</p>
-        <p>{message.text}</p>
+      <div className={`${style.message} ${messageClass}`}>
+        <p className={style.name}>{message.name}</p>
+        <p className="">{message.text}</p>
       </div>
     </div>
   );
